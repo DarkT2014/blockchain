957c5592802e9c55911337822ed12ac887dbc2b1
高铨康 <hzgaoquankang@corp.netease.com>
2018-09-18 00:16:05 +0800

    提交代码

---
+++ demochain/HashDemo.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"log"
+)
+
+func calculateHash(toBeHashed string)  string{
+	hashInBytes := sha256.Sum256([]byte(toBeHashed))
+	hashInStr := hex.EncodeToString(hashInBytes[:])
+	log.Printf("% %",toBeHashed,hashInStr)
+	return hashInStr
+}
+func main()  {
+	calculateHash("test1")
+	calculateHash("test1")
+	calculateHash("test2")
+}
---
+++ demochain/README.md
@@ -0,0 +1,14 @@
+# 构建自己的区块链
+. 实现链式结构
+. 实现一个简单的http server 对外暴露接口
+. 效果展示
+
+# 实现步骤
+. 创建block
+> 新建工程demochain\
+> 创建 Block 文件\
+> 创建block结构体和相关函数
+
+. 创建blockchain
+
+. 创建http server
